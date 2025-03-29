2da6c9c754c9aee1a46e382796dcb0f942003586
다솜 <ektha3869@naver.com>
2025-03-29 18:24:15 +0900

    feat: Add markdown style

--- src/app/components/MdxComponents.tsx
+++ src/app/components/MdxComponents.tsx
@@ -1,10 +1,14 @@
 const MdxComponents = {
-  h1: ({ children }: { children: string }) => <h1 className="text-4xl font-extrabold text-blue-600">{children}</h1>,
-  h2: ({ children }: { children: string }) => <h2 className="text-3xl font-semibold text-green-600">{children}</h2>,
-  h3: ({ children }: { children: string }) => <h3 className="text-2xl font-medium text-red-500">{children}</h3>,
-  h4: ({ children }: { children: string }) => <h4 className="text-xl font-medium text-purple-400">{children}</h4>,
-  h5: ({ children }: { children: string }) => <h5 className="text-lg font-normal text-gray-800">{children}</h5>,
-  h6: ({ children }: { children: string }) => <h6 className="text-base font-normal text-black">{children}</h6>,
+  h1: ({ children }: { children: string }) => (
+    <h1 className="w-100 text-4xl font-extrabold text-blue-600">{children}</h1>
+  ),
+  h2: ({ children }: { children: string }) => (
+    <h2 className="w-100 text-3xl font-semibold text-green-600">{children}</h2>
+  ),
+  h3: ({ children }: { children: string }) => <h3 className="w-100 text-2xl font-medium text-red-500">{children}</h3>,
+  h4: ({ children }: { children: string }) => <h4 className="w-100 text-xl font-medium text-purple-400">{children}</h4>,
+  h5: ({ children }: { children: string }) => <h5 className="w-100 text-lg font-normal text-gray-800">{children}</h5>,
+  h6: ({ children }: { children: string }) => <h6 className="w-100 text-base font-normal text-black">{children}</h6>,
 }
 
 export default MdxComponents
